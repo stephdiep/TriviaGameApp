31ab7eac2fe87fc807ea9854edd46e0092be9dbc
stephdiep <stephanie.diep002@gmail.com>
2021-12-17 10:05:33 -0500

    Initial Commit

---
+++ TriviaGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TriviaGame
+//
+//  Created by Stephanie Diep on 2021-12-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by Stephanie Diep on 2021-12-17.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
